f8e3b32531d2c7a01ff3bac62351d1520f17a076
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-01-03 23:43:27 +0530

    signup and login api

--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,13 @@ const app = require('express')();
 
 // eslint-disable-next-line object-curly-spacing
 const { getAllTodos, deleteTodo, editTodo } = require('./APIs/todos');
-const { loginUser } = require('./APIs/users');
+const { loginUser, signUpUser } = require('./APIs/users');
 
 app.get('/todos', getAllTodos);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
 
 app.post('/login', loginUser);
+app.post('/signup', signUpUser);
 
 exports.api = functions.https.onRequest(app);
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -12,3 +12,36 @@ exports.validateLoginData = (data) => {
     valid: Object.keys(errors).length === 0 ? true : false,
   };
 };
+
+const isEmail = (email) => {
+  const emailRegEx =
+    // eslint-disable-next-line no-useless-escape, max-len
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(emailRegEx)) return true;
+  else return false;
+};
+
+exports.validateSignUpData = (data) => {
+  const errors = {};
+
+  if (isEmpty(data.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(data.email)) {
+    errors.email = 'Must be valid email address';
+  }
+
+  if (isEmpty(data.firstName)) errors.firstName = 'Must not be empty';
+  if (isEmpty(data.lastName)) errors.lastName = 'Must not be empty';
+  if (isEmpty(data.phoneNumber)) errors.phoneNumber = 'Must not be empty';
+  if (isEmpty(data.country)) errors.country = 'Must not be empty';
+
+  if (isEmpty(data.password)) errors.password = 'Must not be empty';
+  // eslint-disable-next-line max-len
+  if (data.password !== data.confirmPassword) errors.confirmPassword = 'Passowrds must be the same';
+  if (isEmpty(data.username)) errors.username = 'Must not be empty';
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0 ? true : false,
+  };
+};
