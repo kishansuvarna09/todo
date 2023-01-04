353b2e48ef4e2a7569f729c9f33c6fd2fca325e0
Kishan Suvarna <kishansuvarna09@gmail.com>
2023-01-04 23:23:33 +0530

    upload profile picture api

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,11 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 
+const auth = require('./util/auth');
+
 // eslint-disable-next-line object-curly-spacing
 const { getAllTodos, deleteTodo, editTodo } = require('./APIs/todos');
-const { loginUser, signUpUser } = require('./APIs/users');
+const { loginUser, signUpUser, uploadProfilePhoto } = require('./APIs/users');
 
 app.get('/todos', getAllTodos);
 app.delete('/todo/:todoId', deleteTodo);
@@ -11,5 +13,7 @@ app.put('/todo/:todoId', editTodo);
 
 app.post('/login', loginUser);
 app.post('/signup', signUpUser);
+
+app.post('/user/image', auth, uploadProfilePhoto);
 
 exports.api = functions.https.onRequest(app);
